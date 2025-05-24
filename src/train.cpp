cc89abb653cdcda2f59ad41df9ad92961512fe6a
malishev-danil <malishev-danil5@yandex.ru>
2025-05-24 15:30:10 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,11 +1,8 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
-
 Train::Train() : first(nullptr), countOp(0) {}
-
 Train::~Train() {
 if (!first) return;
-
 Car* current = first->next;
 while (current != first) {
 Car* toDelete = current;
@@ -14,7 +11,6 @@ delete toDelete;
 }
 delete first;
 }
-
 void Train::addCar(bool light) {
 Car* newCar = new Car(light);
 if (!first) {
@@ -29,12 +25,9 @@ newCar->next = first;
 first->prev = newCar;
 }
 }
-
 int Train::getLength() {
 countOp = 0;
-
 if (!first) return 0;
-
 Car* current = first;
 bool hasLight = false;
 do {
@@ -44,39 +37,33 @@ break;
 }
 current = current->next;
 } while (current != first);
-
 if (!hasLight) {
 first->light = true;
-Car* iterator = first->next;
+const Car* iterator = first->next;
 ++countOp;
 int length = 1;
-
 while (iterator != first) {
 iterator = iterator->next;
 ++countOp;
 ++length;
 }
-
 iterator = iterator->prev;
 for (int i = 0; i < length; ++i) {
 iterator = iterator->prev;
 ++countOp;
 }
-
 first->light = false;
 return length;
 } else {
-
 Car* iterator = first->next;
 ++countOp;
 int length = 1;
-
 while (iterator != first) {
 iterator = iterator->next;
 ++countOp;
 ++length;
 }
-countOp += static_cast<long long>(length) * length;
+countOp += static_cast<int64_t>(length) * length;
 return length;
 }
 }
