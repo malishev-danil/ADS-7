87c98345e18b3b1159f4daf9c7371941acc288d6
malishev-danil <malishev-danil5@yandex.ru>
2025-05-24 11:41:25 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -1,24 +1,24 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TRAIN_H_
 #define INCLUDE_TRAIN_H_
-
+#include <cstdint>
 class Train {
- private:
-  struct Car {
-    bool light; // состояние лампочки
-    Car *next;
-    Car *prev;
-    explicit Car(bool l == false) : light(l), next(this), prev(this) {}
-  };
-  int countOp; // счетчик шагов (число переходов из вагона в вагон)
-  Car *first; // точка входа в поезд (первый вагон)
+private:
+struct Car {
+bool light; // состояние лампочки
+Car *next;
+Car *prev;
+explicit Car(bool l == false) : light(l), next(this), prev(this) {}
+};
+int countOp; // счетчик шагов (число переходов из вагона в вагон)
+Car *first; // точка входа в поезд (первый вагон)
 };
 
- public:
-  Train();
-   ~Train();
-  void addCar(bool light); // добавить вагон с начальным состоянием лампочки
-  int getLength();          // вычислить длину поезда
-  int getOpCount() const { return countOp; }         // вернуть число переходов (из вагона в вагон)
+public:
+Train();
+~Train();
+void addCar(bool light); // добавить вагон с начальным состоянием лампочки
+int getLength();          // вычислить длину поезда
+int getOpCount() const { return countOp; }         // вернуть число переходов (из вагона в вагон)
 };
 #endif  // INCLUDE_TRAIN_H_
